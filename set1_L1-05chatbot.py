184bb6a579d43e23044834a672d99e3b5e40f8a1
PRAFUL DAGADU PATIL <patilpraful4370@gmail.com>
2026-09-20 19:55:08 +0530

    Update set1_L1-05chatbot.py

--- set1_L1-05chatbot.py
+++ set1_L1-05chatbot.py
@@ -1,7 +1,6 @@
-#---------------------Set 1 – L1‑05 Observability & Logging-------------------
 
 import time
-from logger import log_interaction   # make sure logger.py exists
+from logger import log_interaction   
 
 def fake_ai_response(prompt: str) -> str:
     return f"Summary of: {prompt[:50]}..."
@@ -23,8 +22,4 @@ if __name__ == "__main__":
 
         log_interaction(user_input, response, latency, tokens_used)
 
-#---how to run --
-#--open power shell and type  cd "C:\Users\INDIA\AI Developer Assignment"
-#-then go to vs code--- type into You-- How are you?. 
-#- chatbot is execute
-#--- python set1_L1-05chatbot.py
+
